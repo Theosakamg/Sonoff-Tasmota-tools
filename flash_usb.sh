8e49b58811f360cd7d2e67c2776cd5c845fe55ec
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2019-11-04 10:12:09 +0100

    Clean code

--- flash_usb.sh
+++ flash_usb.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+#==============================================================================
+#Title           :flash_usb.sh
+#Description     :This script will flash Sonoff device with custom firmware.
+#Author          :Mickael Gaillard <mick.gaillard@gmail.com>
+#Date            :20191103
+#Version         :0.1
+#Usage           :bash flash_usb.sh --usb /dev/ttyUSB0 --file sonoff.bin
+#==============================================================================
 
 set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
@@ -9,71 +17,73 @@ readonly script_name=$(basename "${0}")
 readonly script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 IFS=$'\t\n'   # Split on newlines and tabs (but not on spaces)
 
-
-# set initial values
+# set initial values.
 VERBOSE=false
 USB=/dev/ttyUSB0
 FILE_FW=sonoff.bin
 SIZE_FLASH=1MB
 CMD=esptool
 
-# Option strings
-SHORT=vu:f:s:c:
-LONG=verbose,usb:,file:,size:,cmd:
+parse() {
+# Option strings.
+  SHORT=vu:f:s:c:
+  LONG=verbose,usb:,file:,size:,cmd:
 
-# read the options
-OPTS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
-if [ $? != 0 ] ; then echo "Failed to parse options...exiting." >&2 ; exit 1 ; fi
-eval set -- "$OPTS"
+  # read the options.
+  OPTS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
+  if [ $? != 0 ] ; then echo "Failed to parse options...exiting." >&2 ; exit 1 ; fi
+  eval set -- "$OPTS"
 
-# extract options and their arguments into variables.
-while true ; do
-  case "$1" in
-    -v | --verbose )
-      VERBOSE=true
-      shift
-      ;;
-    -u | --usb )
-      USB="$2"
-      shift 2
-      ;;
-    -f | --file )
-      FILE_FW="$2"
-      shift 2
-      ;;
-    -s | --size )
-      SIZE_FLASH="$2"
-      shift 2
-      ;;
-    -c | --cmd )
-      CMD="$2"
-      shift 2
-      ;;
-    -- )
-      shift
-      break
-      ;;
-    *)
-      echo "Internal error!"
-      exit 1
-      ;;
-  esac
-done
+  # extract options and their arguments into variables.
+  while true ; do
+    case "$1" in
+      -v | --verbose )
+        VERBOSE=true
+        shift
+        ;;
+      -u | --usb )
+        USB="$2"
+        shift 2
+        ;;
+      -f | --file )
+        FILE_FW="$2"
+        shift 2
+        ;;
+      -s | --size )
+        SIZE_FLASH="$2"
+        shift 2
+        ;;
+      -c | --cmd )
+        CMD="$2"
+        shift 2
+        ;;
+      -- )
+        shift
+        break
+        ;;
+      *)
+        echo "Internal error!"
+        exit 1
+        ;;
+    esac
+  done
 
-if [ ! -e "$USB" ]; then
-  echo "Error:USB at ${USB} not found" >&2
-  exit 1
-fi
+  # check variables.
+  if [ ! -e "$USB" ]; then
+    echo "Error: USB at ${USB} not found" >&2
+    exit 1
+  fi
 
-if [ ! -e "$FILE_FW" ]; then
-  echo "Error:Firmware ${FILE_FW} not found" >&2
-  exit 1
-fi
+  if [ ! -e "$FILE_FW" ]; then
+    echo "Error: Firmware ${FILE_FW} not found" >&2
+    exit 1
+  fi
 
-if ! [ -x "$(command -v "$CMD")" ]; then
-  echo "Error: ${CMD} is not installed or found." >&2
-  exit 1
-fi
+  if ! [ -x "$(command -v "$CMD")" ]; then
+    echo "Error: ${CMD} is not installed or found." >&2
+    exit 1
+  fi
+}
 
 main() {
   echo -e "GET MAC (for filename)...\n"
@@ -98,4 +108,5 @@ main() {
   echo "====================================" >> $FILE
 }
 
-main "${@}"
+parse "${@}"
+main
