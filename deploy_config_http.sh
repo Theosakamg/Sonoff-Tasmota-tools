c1f8104c70ef50055766eeec1633652004e20b64
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2019-12-28 21:57:30 +0100

    Add deploy config file
    Signed-off-by: Mickael Gaillard <mickael.gaillard@tactfactory.com>

--- deploy_config_http.sh
+++ deploy_config_http.sh
@@ -1,39 +1,57 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#==============================================================================
+#Title           :deploy_config_http.sh
+#Description     :This script will deploy configuration on Sonoff device.
+#Author          :Mickael Gaillard <mick.gaillard@gmail.com>
+#Date            :20191103
+#Version         :0.1
+#Usage           :bash deploy_config_http.sh
+#==============================================================================
 
-## Main config
-IP_DEV=
+# Global options.
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+# set -o xtrace
 
-DEVICE_DOMAIN=
-DEVICE_NAME=
-DEVICE_TYPE=
-DEVICE_ROOM=
+readonly script_name=$(basename "${0}")
+readonly script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+IFS=$'\t\n'   # Split on newlines and tabs (but not on spaces)
+
+# Set initial values.
+IP_DEV=IP
+
+DEVICE_DOMAIN=domain
+DEVICE_NAME=device
+DEVICE_TYPE=light
+DEVICE_ROOM=room
 DEVICE_USER=
 DEVICE_PASS=
-DEVICE_FULLNAME="$DEVICE_NAME-$DEVICE_TYPE.$DEVICE_ROOM"
-DEVICE_HOSTNAME="$DEVICE_NAME-$DEVICE_TYPE.$DEVICE_ROOM" # Replace "%s-%04d"
 
 # Save state and aplly after reboot
 SAVE_SATE=1
+
 # Not add unit on result value
 NO_UNIT=0
+
 # Display value on Fahrenheit (or Celsius)
 UNIT_FAR=0
 
 DISPLAY_ENABLE=1
 
 ## DEFINE MANUALY ! (NOT USE FROM DHCP)
-NTP_SRV1=
-NTP_SRV2=pool.ntp.org
+NTP_SRV1=pool.ntp.org
+NTP_SRV2=0
 
 ## SERIAL CONFIG
 SER_BAUD=9600
 
 ## MQTT
 MQTT_ENABLE=1
-MQTT_HOST=
+MQTT_HOST=0
 MQTT_PORT=1883
-MQTT_USER=
-MQTT_PASS=
+MQTT_USER=guest
+MQTT_PASS=guest
 MQTT_CLIENT=DVES_%06X
 MQTT_GRP=all
 MQTT_TOPIC="$DEVICE_NAME"
@@ -45,6 +63,30 @@ MQTT_MSG_ON=ON
 MQTT_MSG_TOGGLE=TOGGLE
 MQTT_MSG_HOLD=HOLD
 
+# Internal functions
+
+help() {
+  echo -e "\nbash deploy_config_http.sh [opt1] [opt1] [...]"
+  echo "Options :"
+  echo -e " -h | --help\t\t\t: this help message."
+  echo -e "\neg. : bash deploy_config_http.sh"
+  echo -e "\nOR You can config all options in deploy.config file."
+}
+
+readFile() {
+  # Read config from file.
+  if [ -f ${script_dir}/deploy.config ]; then
+    echo "Read config from file..."
+    . ${script_dir}/deploy.config
+  fi
+}
+
+readFile
+
+
+DEVICE_FULLNAME="$DEVICE_NAME-$DEVICE_TYPE.$DEVICE_ROOM"
+DEVICE_HOSTNAME="$DEVICE_NAME-$DEVICE_TYPE.$DEVICE_ROOM" # Replace "%s-%04d"
+
 ## https://github.com/arendst/Sonoff-Tasmota/wiki/Commands
 ACTIONS_NO_REBOOT="\
  SetOption0 $SAVE_SATE;\
@@ -55,14 +97,14 @@ ACTIONS_NO_REBOOT="\
  StateText2 $MQTT_MSG_ON;\
  StateText3 $MQTT_MSG_TOGGLE;\
  StateText4 $MQTT_MSG_HOLD;\
- PowerOnState 3;\
+ PowerOnState $POWERONSTATE;\
  IPAddress1 0.0.0.0;\
  FriendlyName $DEVICE_FULLNAME;\
  Hostname $DEVICE_HOSTNAME;\
  WebPassword $DEVICE_PASS;\
  WifiConfig 4;\
 "
-### REMOVE COMMAND
+# REMOVE COMMAND
 #  SetOption2 $NO_UNIT;\
 
 ACTIONS_WITH_REBOOT="\
